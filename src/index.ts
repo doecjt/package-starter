06acc252c8b1567e4a68aa98b1558ad672e980c9
doecjt <doecjt@gmail.com>
2023-06-06 15:34:00 +0800

    feat: lint-staged

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { add } from './math.js'
 const sum = add(1, 2)
 const root = document.querySelector('#app')
 root && (root.textContent = `${sum}`)
-console.log('sum', sum)
+console.log('sum1', sum)
